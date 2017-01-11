4981e440fafa88687fb97b6c7f2efd21fb22b565
Theodor Vararu <theo@vararu.org>
2017-01-11 14:32:57 +0000

    Make example 4 a typeahead

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,19 @@ router.get('/', function (req, res) {
 
 // add your routes here
 router.get('/country-picker-4', function (req, res) {
-  res.render('country-picker-4', { locations: locationService.canonicalLocationList })
+  res.render('country-picker-4', {
+    graph: locationService.locationGraph,
+    locations: locationService.canonicalLocationList,
+    reverseMap: locationService.locationReverseMap,
+    locale: 'en-GB'
+  })
+})
+
+router.get('/country-picker-4.cy', function (req, res) {
+  res.render('country-picker-4', {
+    locations: locationService.canonicalLocationList,
+    locale: 'cy'
+  })
 })
 
 module.exports = router
